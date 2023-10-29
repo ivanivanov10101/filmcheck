4e3d838ad9e10a9430089c69356c2ff0a51ab903
Ivan Ivanov <78234071+ivanivanov10101@users.noreply.github.com>
2023-10-29 16:52:16 +0100

    theme fixes

--- src/components/nav/ThemeToggle.js
+++ src/components/nav/ThemeToggle.js
@@ -1,17 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import LightModeIcon from '@mui/icons-material/LightMode';
 
 export default function ThemeToggle(props) {
+  const [icon, setIcon] = useState('DarkModeIcon')
+  const themeIsLight = (icon === 'LightModeIcon');
+  const Icon = themeIsLight ? <LightModeIcon /> : <DarkModeIcon />
   return (
-    <button className="theme-toggle">
-      <img
-        onClick={props.toggleDarkMode}
-        src={
-          props.darkMode
-            ? "../data/toggle-icon-dark.jpg"
-            : "../.././public/assets/toggle-icon-dark.jpg"
-        }
-        alt="dark icon"
-      />
+    <button className="theme-toggle button-8" onClick={props.toggleDarkMode}>
+      {/*<img*/}
+      {/*  onClick={props.toggleDarkMode}*/}
+      {/*  src={*/}
+      {/*    props.darkMode*/}
+      {/*      ? <DarkModeIcon/>*/}
+      {/*      : "../.././public/assets/toggle-icon-dark.jpg"*/}
+      {/*  }*/}
+      {/*  alt="dark icon"*/}
+      {/*/>*/}
+      {Icon}
     </button>
   );
 }
