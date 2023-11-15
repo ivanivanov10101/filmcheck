9e47115a24a570511d55516a8f3e460de52a92ee
Ivan Ivanov <78234071+ivanivanov10101@users.noreply.github.com>
2023-11-15 20:23:46 +0100

    initial react-select dropdown

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # filmcheck
  A repository containing the FilmCheck personal project.
 
+ Built with ReactJS, React Router, Tanstack Query.  
+ Hosted with Vercel.
+
 ![preview1](preview1.png)
 ![preview3](preview3.png)
 ![preview2](preview2.png)
--- src/components/shared/HeaderDropDown.js
+++ src/components/shared/HeaderDropDown.js
@@ -1,12 +1,61 @@
+import Select from "react-select";
+
 const HeaderDropDown = () => {
+  const options = [
+    { value: "Popular Movies", label: "Popular Movies" },
+    { value: "Top Rated", label: "Top Rated" },
+    { value: "Now Playing", label: "Now Playing" },
+  ];
+
   return (
-    <select className="films-dropdown">
-      <option value="Popular Movies">Popular Movies</option>
+    <div style={{ width: "20rem" }}>
+      <Select
+        styles={{
+          option: (baseStyles, state) => ({
+            ...baseStyles,
+            color: "white",
+            width: "inherit",
+            background: state.isFocused ? "#7ad76a" : "#18c50f",
+            fontFamily: "Roboto"
+          }),
+          control: (baseStyles, state) => ({
+            ...baseStyles,
+            backgroundColor: "transparent",
+            width: "inherit",
+            boxShadow: state.isFocused ? null : null,
+            borderColor: state.isFocused ? "transparent" : "transparent",
+            "&:hover": {
+              borderColor: state.isFocused ? "transparent" : "transparent"
+            }
+          }),
+          menuList: (baseStyles, state) => ({
+            ...baseStyles,
+            padding: 0,
+            borderRadius: 5,
+            boxShadow: "0 3px 55px 1px black"
 
-      <option value="Top Rated">Top Rated</option>
+          }),
+          menu: (baseStyles, state) => ({
+            ...baseStyles,
 
-      <option value="Now Playing">Now Playing</option>
-    </select>
+          }),
+        }}
+        theme={(theme) => ({
+          ...theme,
+          colors: {
+            ...theme.colors,
+            primary: "#7ad76a",
+            neutral80: "#7ad76a",
+          },
+        })}
+        options={options}
+        components={{
+          IndicatorSeparator: () => null
+        }}
+        isSearchable={false}
+        defaultValue={options[0]}
+      />
+    </div>
   );
 };
 export default HeaderDropDown;
