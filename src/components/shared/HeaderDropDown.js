07b1db2eca1bb5013fcf79f139ebce6edf74e58c
Ivan Ivanov <78234071+ivanivanov10101@users.noreply.github.com>
2023-11-15 23:05:04 +0100

    minor fixes

--- src/components/shared/HeaderDropDown.js
+++ src/components/shared/HeaderDropDown.js
@@ -8,7 +8,7 @@ const HeaderDropDown = () => {
   ];
 
   return (
-    <div style={{ width: "20rem" }}>
+    <div style={{ width: "20rem"}}>
       <Select
         styles={{
           option: (baseStyles, state) => ({
@@ -16,7 +16,8 @@ const HeaderDropDown = () => {
             color: "white",
             width: "inherit",
             background: state.isFocused ? "#7ad76a" : "#18c50f",
-            fontFamily: "Roboto"
+            fontFamily: "Roboto Light, sans-serif",
+            fontSize: "0.85em",
           }),
           control: (baseStyles, state) => ({
             ...baseStyles,
@@ -26,7 +27,9 @@ const HeaderDropDown = () => {
             borderColor: state.isFocused ? "transparent" : "transparent",
             "&:hover": {
               borderColor: state.isFocused ? "transparent" : "transparent"
-            }
+            },
+            fontSize: "1em",
+            fontFamily: "Roboto Light, sans-serif",
           }),
           menuList: (baseStyles, state) => ({
             ...baseStyles,
